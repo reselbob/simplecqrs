88a29859720f3b65d2b769864c5b64bc2130ee72
Bob Reselman <reselbob@gmail.com>
2020-12-29 16:15:15 -0800

    adding seed to 2

---
+++ utilities/seeder/Seeder.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import {OrderInput} from "../../src/write_db/inputs/OrderInput";
+import {TestUtility} from "../testUtility/TestUtility";
+
+export class Seeder {
+    private targetUrl: string;
+
+    constructor(targetUrl: string) {
+        this.targetUrl = targetUrl;
+    }
+
+    public async seed(numberOfItems: number): Promise<void> {
+        if (! numberOfItems ) {
+            throw new Error("Seeder.seed(numberOfItems) is missing a value for the numberOfItems parameter");
+        }
+        // tslint:disable-next-line:no-console
+        console.log(`Starting seeding for ${numberOfItems} orders at ${new Date()}`);
+        for (let i = 0; i < numberOfItems; i++) {
+            const order: OrderInput = TestUtility.createGenericOrderSync();
+            await axios.post(this.targetUrl, order);
+            // tslint:disable-next-line:no-console
+            console.log(`Seeded ${JSON.stringify(order)}  at ${new Date()}`);
+
+        }
+        // tslint:disable-next-line:no-console
+        console.log(`Finished seeding at ${new Date()}`);
+
+    }
+}
---
+++ utilities/seeder/index.ts
@@ -0,0 +1,14 @@
+import {Seeder} from "./Seeder";
+
+const targetUrl = process.argv[2].split("=")[1];
+
+// tslint:disable-next-line:no-console
+console.log(targetUrl);
+
+const seeder = new Seeder(targetUrl);
+
+seeder.seed(10)
+    .then(() => {
+        // tslint:disable-next-line:no-console
+        console.log("seeding complete");
+    });
